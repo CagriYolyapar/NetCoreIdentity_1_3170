e94a81ad068243c624b0b75ab76fcb0197575c25
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-03-14 13:29:17 +0300

    Role ve UserCreate islemleri tamamlandı

--- NetCoreIdentity_1/Controllers/UserController.cs
+++ NetCoreIdentity_1/Controllers/UserController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NetCoreIdentity_1.Models.Admins.AppRoles.PageVms;
+using NetCoreIdentity_1.Models.Admins.AppRoles.ResponseModels;
+using NetCoreIdentity_1.Models.Admins.AppUsers.RequestModels;
 using NetCoreIdentity_1.Models.Entities;
 
 namespace NetCoreIdentity_1.Controllers
@@ -28,13 +31,82 @@ namespace NetCoreIdentity_1.Controllers
             return View(nonAdminUsers);
         }
 
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserRequestModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, $"{model.UserName}cgr123");
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(appUser, "Member");
+                    return RedirectToAction("Index");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View(model);
+        }
+
+
         public async Task<IActionResult> AssignRole(int id)
         {
             AppUser appUser = await _userManager.Users.SingleOrDefaultAsync(x => x.Id == id);
 
-            IList<string> userRoles = await  _userManager.GetRolesAsync(appUser); // Elimize gecen kullanıcının rollerini verir
+            IList<string> userRoles = await _userManager.GetRolesAsync(appUser); // Elimize gecen kullanıcının rollerini verir
 
             List<AppRole> allRoles = _roleManager.Roles.ToList(); //bütün roller
+
+            List<AppRoleResponseModel> roles = new();
+            
+
+            foreach (AppRole role in allRoles)
+            {
+                roles.Add(new()
+                {
+                    RoleID = role.Id,
+                    RoleName = role.Name,
+                    Checked = userRoles.Contains(role.Name)
+                });
+            }
+
+            AssignRolePageVM arPvm = new()
+            {
+                UserID = id,
+                Roles = roles
+            };
+
+            return View(arPvm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(AssignRolePageVM model)
+        {
+            AppUser appUser = await _userManager.Users.SingleOrDefaultAsync(x => x.Id == model.UserID);
+            IList<string> userRoles = await _userManager.GetRolesAsync(appUser);
+
+            foreach (AppRoleResponseModel role in model.Roles)
+            {
+                if (role.Checked && !userRoles.Contains(role.RoleName)) await _userManager.AddToRoleAsync(appUser, role.RoleName);
+                else if(!role.Checked && userRoles.Contains(role.RoleName)) await _userManager.RemoveFromRoleAsync(appUser, role.RoleName);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
